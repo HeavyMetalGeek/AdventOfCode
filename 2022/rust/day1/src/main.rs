cd29aecea1b4896f9e04bd5996f84dbe21519ba0
HeavyMetalGeek <fatirishman53@gmail.com>
2022-12-03 14:56:08 -0600

    solution complete

---
+++ 2022/rust/day1/src/main.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use std::env;
+use std::fs::read_to_string;
+
+enum Input {
+    Test,
+    Answer,
+}
+
+impl Input {
+    fn value(&self) -> &str {
+        match *self {
+            Input::Test => "input/test.txt",
+            Input::Answer => "input/stage1.txt",
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    for arg in args {
+        match arg.as_str() {
+            "stage1" | "1" => {
+                println!("{}", stage1(Input::Answer)?.iter().max().unwrap_or(&0));
+                return Ok(());
+            }
+            "stage2" | "2" => {
+                println!("{}", stage2(Input::Answer)?);
+                return Ok(());
+            }
+            "test1" => {
+                println!("{}", stage1(Input::Test)?.iter().max().unwrap_or(&0));
+                return Ok(());
+            }
+            "test2" => {
+                println!("{}", stage2(Input::Test)?);
+                return Ok(());
+            }
+            _ => continue,
+        }
+    }
+    println!("You must choose a run mode");
+    Ok(())
+}
+
+fn stage1(input: Input) -> Result<Vec<usize>> {
+    let txt: String = match input {
+        Input::Test => read_to_string(Input::Test.value())?.parse()?,
+        Input::Answer => read_to_string(Input::Answer.value())?.parse()?,
+    };
+    Ok(txt
+        .split("\n\n")
+        .map(|s| {
+            s.split('\n')
+                .map(|c| c.to_string().parse::<usize>().unwrap_or(0))
+                .sum::<usize>()
+        })
+        .collect())
+}
+
+fn stage2(input: Input) -> Result<usize> {
+    let mut stage1 = stage1(input)?;
+    stage1.sort_by(|a, b| b.partial_cmp(a).expect("Sort failed"));
+    println!(
+        "{}",
+        stage1
+            .iter()
+            .map(|v| format!("{}", v))
+            .collect::<Vec<String>>()
+            .join(",")
+    );
+    Ok(stage1.into_iter().take(3).sum::<usize>())
+}
