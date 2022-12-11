7a0f82fcc2348deaabdca56e6539c91fc3835bc8
HeavyMetalGeek <fatirishman53@gmail.com>
2022-12-10 18:28:42 -0600

    day 3, stage 2 complete

--- 2022/rust/src/bin/day3.rs
+++ 2022/rust/src/bin/day3.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use aoc::input::Input;
 use std::{collections::HashSet, env, str::FromStr};
 
@@ -62,6 +62,41 @@ impl FromStr for Rucksack {
     }
 }
 
+pub struct ElfGroup {
+    pub badge: char,
+    pub priority: usize,
+}
+
+impl ElfGroup {
+    fn from_strings(s: &[&str]) -> Result<Self> {
+        if s.len() < 3 {
+            return Err(anyhow!("Elf group must be composed from 3 strings."));
+        }
+        let ruck1 = HashSet::from_iter(s[0].chars());
+        let ruck2 = HashSet::from_iter(s[1].chars());
+        let ruck3 = HashSet::from_iter(s[2].chars());
+        let badge = Self::common_char(ruck1, ruck2, ruck3)?;
+        let priority = match badge as u32 {
+            x if (65..=90).contains(&x) => (x - 38) as usize,
+            x if (97..=122).contains(&x) => (x - 96) as usize,
+            _ => 0 as usize,
+        };
+
+        Ok(Self { badge, priority })
+    }
+
+    fn common_char(
+        ruck1: HashSet<char>,
+        ruck2: HashSet<char>,
+        ruck3: HashSet<char>,
+    ) -> Result<char> {
+        ruck1
+            .into_iter()
+            .find(|c| ruck2.contains(c) && ruck3.contains(c))
+            .ok_or_else(|| anyhow!("No badge char found"))
+    }
+}
+
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     for arg in args {
@@ -71,7 +106,7 @@ fn main() -> Result<()> {
                 return Ok(());
             }
             "stage2" | "2" => {
-                println!("{}", 0);
+                println!("{}", stage2(Input::Answer)?);
                 return Ok(());
             }
             "test1" => {
@@ -79,7 +114,7 @@ fn main() -> Result<()> {
                 return Ok(());
             }
             "test2" => {
-                println!("{}", 0);
+                println!("{}", stage2(Input::Test)?);
                 return Ok(());
             }
             _ => continue,
@@ -98,8 +133,25 @@ fn stage1(input: Input) -> Result<usize> {
     Ok(rucks.iter().map(|r| r.priority).sum())
 }
 
+fn stage2(input: Input) -> Result<usize> {
+    let rucks: Vec<ElfGroup> = input
+        .into_string(DAY)?
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|v| ElfGroup::from_strings(v).unwrap())
+        .collect();
+    Ok(rucks.iter().map(|r| r.priority).sum())
+}
+
 #[test]
 fn stage1_test() {
     let res = stage1(Input::Test).unwrap();
     assert_eq!(res, 157);
+}
+
+#[test]
+fn stage2_test() {
+    let res = stage2(Input::Test).unwrap();
+    assert_eq!(res, 70);
 }
