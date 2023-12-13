3e78b4289c1d11710b460305c87093ad5a68ff9f
Joseph McCartney <mccartneyjwh@gmail.com>
2023-12-13 17:03:20 -0600

    day 7 complete

--- 2023/Python/day_7/day_7_solution.py
+++ 2023/Python/day_7/day_7_solution.py
@@ -12,126 +12,181 @@ def get_lines(file: str):
         return lines
 
 
-def compute_distance(race_duration: int, hold_time: int):
-    if hold_time < 0 or hold_time > race_duration:
-        return -1e9  # Penalty for going out of bounds
+class Hand:
+    card_ranking = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    }
+    card_ranking_2 = card_ranking.copy()
+    card_ranking_2["J"] = 1
 
-    speed = hold_time
-    distance = (race_duration - hold_time) * speed
+    def __init__(self, line: str):
+        cards, bid = line.split(" ")
+        self.cards = cards
+        self.bid = int(bid)
 
-    return distance  # mm traveled
+    def __str__(self):
+        return f"{self.cards}"
+
+    def get_rank(self) -> int:
+        buckets = {}
+        for card in self.cards:
+            if card in buckets.keys():
+                buckets[card] += 1
+            else:
+                buckets[card] = 1
+        asdf = [d for d in buckets.values()]
+        asdf.sort(reverse=True)
+
+        if asdf[0] == 5:
+            # print("five of a kind")
+            return 7
+        if asdf[0] == 4 and asdf[1] == 1:
+            # print("four of a kind")
+            return 6
+        if asdf[0] == 3 and asdf[1] == 2:
+            # print("full house")
+            return 5
+        if asdf[0] == 3 and asdf[1] == 1 and asdf[1] == 1:
+            # print("three of a kind")
+            return 4
+        if asdf[0] == 2 and asdf[1] == 2 and asdf[2] == 1:
+            # print("two of a kind")
+            return 3
+        if asdf[0] == 2 and asdf[1] == 1 and asdf[2] == 1 and asdf[3] == 1:
+            # print("one pair")
+            return 2
+        else:
+            # print("high card")
+            return 1
+
+    def get_rank_2(self) -> int:
+        buckets = {}
+        for card in self.cards:
+            if card in buckets.keys():
+                buckets[card] += 1
+            else:
+                buckets[card] = 1
+
+        if "J" in buckets.keys():
+            max_value = 0
+            mak_key = None
+            for k, v in buckets.items():
+                if k != "J" and v > max_value:
+                    max_value = v
+                    max_key = k
+
+            # Protect against "JJJJJ"
+            if len(buckets) > 1:
+                buckets[max_key] = buckets["J"] + buckets[max_key]
+                del buckets["J"]
+
+        asdf = [d for d in buckets.values()]
+        asdf.sort(reverse=True)
+
+        if asdf[0] == 5:
+            # print("five of a kind")
+            return 7
+        if asdf[0] == 4 and asdf[1] == 1:
+            # print("four of a kind")
+            return 6
+        if asdf[0] == 3 and asdf[1] == 2:
+            # print("full house")
+            return 5
+        if asdf[0] == 3 and asdf[1] == 1 and asdf[1] == 1:
+            # print("three of a kind")
+            return 4
+        if asdf[0] == 2 and asdf[1] == 2 and asdf[2] == 1:
+            # print("two of a kind")
+            return 3
+        if asdf[0] == 2 and asdf[1] == 1 and asdf[2] == 1 and asdf[3] == 1:
+            # print("one pair")
+            return 2
+        else:
+            # print("high card")
+            return 1
+
+
+def compare(hand1: Hand, hand2: Hand) -> int:
+    rank1 = hand1.get_rank()
+    rank2 = hand2.get_rank()
+
+    if rank1 < rank2:
+        return -1
+    elif rank1 > rank2:
+        return 1
+    else:
+        for a, b in zip(hand1.cards, hand2.cards):
+            if hand1.card_ranking[a] < hand2.card_ranking[b]:
+                return -1
+            elif hand1.card_ranking[a] > hand2.card_ranking[b]:
+                return 1
+
+        return 0  # totally identical, probably comparing self (why python???)
+
+
+def compare_2(hand1: Hand, hand2: Hand) -> int:
+    rank1 = hand1.get_rank_2()
+    rank2 = hand2.get_rank_2()
+
+    if rank1 < rank2:
+        return -1
+    elif rank1 > rank2:
+        return 1
+    else:
+        for a, b in zip(hand1.cards, hand2.cards):
+            if hand1.card_ranking_2[a] < hand2.card_ranking_2[b]:
+                return -1
+            elif hand1.card_ranking_2[a] > hand2.card_ranking_2[b]:
+                return 1
+
+        return 0  # totally identical, probably comparing self (why python???)
 
 
 def main(file: str, part: int):
     lines = get_lines(file)
-    # List comprehension to get two lists of integers
-    # there's probably some zip method to do these both at the same time
-    race_times = [int(d) for d in lines[0].split(":")[1].split()]
-    record_distances = [int(d) for d in lines[1].split(":")[1].split()]
 
     if part == 1:
-        # # Confirm I get the same output as the test input
-        # for i in range(7):
-        #     print(compute_distance(7, i))
+        hands = []
+        for line in lines:
+            hand = Hand(line)
+            hands.append(hand)
 
-        win_margins = []
-        for race_duration, record_distance in zip(race_times, record_distances):
-            # We want to maximize the distance, but optimize minimizes the function, so -1 to invert
-            optimal_func = lambda x: compute_distance(race_duration, x) * -1
+        hands.sort(key=functools.cmp_to_key(compare))
 
-            # Search for the optimal hold time
-            optimized_hold = opt.minimize(optimal_func, 1)
+        total_winnings = 0
+        for index, hand in enumerate(hands):
+            total_winnings += hand.bid * (index + 1)
 
-            # Get the integer hold time that results in the largest distance
-            if optimal_func(math.floor(optimized_hold.x)) >= optimal_func(
-                math.ceil(optimized_hold.x)
-            ):
-                optimized_hold = math.floor(optimized_hold.x)
-            else:
-                optimized_hold = math.ceil(optimized_hold.x)
-
-            # split thee race on optimal time, and search the lower half for the record time
-            low = 1
-            high = optimized_hold - 0.1
-            while low <= high:
-                mid = low + (high - low) / 2
-                distance = compute_distance(race_duration, mid)
-                if record_distance - distance < 0:
-                    high = mid - 0.000001
-                else:
-                    low = mid + 0.000001
-
-            hold_lower = math.ceil(mid)
-
-            # split the race on optimal time, and search the upper half for the record time
-            low = optimized_hold + 0.1
-            high = race_duration - 1
-            while low <= high:
-                mid = low + (high - low) / 2
-                distance = compute_distance(race_duration, mid)
-                if record_distance - distance > 0:
-                    high = mid - 0.000001
-                else:
-                    low = mid + 0.000001
-
-            hold_upper = math.floor(mid)
-
-            # compute the win margin and store for later
-            win_margins.append(hold_upper - hold_lower + 1)
-
-        # multiply all the elements together to compute the answer
-        print(f"Part 1 = {np.prod(win_margins)}")
+        print("Part 1 total winnings =", total_winnings)
     if part == 2:
-        race_duration = int("".join(lines[0].split(":")[1].split()))
-        record_distance = int("".join(lines[1].split(":")[1].split()))
+        hands = []
+        for line in lines:
+            hand = Hand(line)
+            hands.append(hand)
 
-        # copy pasta, because i'm lazy
-        # We want to maximize the distance, but optimize minimizes the function, so -1 to invert
-        optimal_func = lambda x: compute_distance(race_duration, x) * -1
+        hands.sort(key=functools.cmp_to_key(compare_2))
 
-        # Search for the optimal hold time
-        optimized_hold = opt.minimize(optimal_func, 1)
+        total_winnings = 0
+        for index, hand in enumerate(hands):
+            total_winnings += hand.bid * (index + 1)
 
-        # Get the integer hold time that results in the largest distance
-        if optimal_func(math.floor(optimized_hold.x)) >= optimal_func(
-            math.ceil(optimized_hold.x)
-        ):
-            optimized_hold = math.floor(optimized_hold.x)
-        else:
-            optimized_hold = math.ceil(optimized_hold.x)
-
-        # split thee race on optimal time, and search the lower half for the record time
-        low = 1
-        high = optimized_hold - 0.1
-        while low <= high:
-            mid = low + (high - low) / 2
-            distance = compute_distance(race_duration, mid)
-            if record_distance - distance < 0:
-                high = mid - 0.000001
-            else:
-                low = mid + 0.000001
-
-        hold_lower = math.ceil(mid)
-
-        # split the race on optimal time, and search the upper half for the record time
-        low = optimized_hold + 0.1
-        high = race_duration - 1
-        while low <= high:
-            mid = low + (high - low) / 2
-            distance = compute_distance(race_duration, mid)
-            if record_distance - distance > 0:
-                high = mid - 0.000001
-            else:
-                low = mid + 0.000001
-
-        hold_upper = math.floor(mid)
-
-        # multiply all the elements together to compute the answer
-        print(f"Part 2 = {hold_upper - hold_lower + 1}")
+        print("Part 2 total winnings =", total_winnings)
 
 
 if __name__ == "__main__":
-    main("day_6_test_input.txt", 1)
-    main("day_6_input.txt", 1)
-    main("day_6_test_input.txt", 2)
-    main("day_6_input.txt", 2)
+    main("day_7_test_input.txt", 1)
+    main("day_7_input.txt", 1)
+    main("day_7_test_input.txt", 2)
+    main("day_7_input.txt", 2)
