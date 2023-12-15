9dca1831b475ed482520d887f881bb7662397ccb
Heathtech <heathbar1989@hotmail.com>
2023-12-15 09:34:32 -0600

    Day 14 Rough

--- 2023/kotlin/Day14/src/Part2.kt
+++ 2023/kotlin/Day14/src/Part2.kt
@@ -1,22 +1,52 @@
 fun main() {
     getResourceAsText("/input.txt")?.lines()?.map { it.toCharArray() }?.run {
-        rotate().sumOf { line ->
-            var freeSpace = 0
-            line.withIndex().sumOf { (x, c) -> when(c) {
-                'O' -> size - freeSpace++
-                '#' -> 0.also { freeSpace = x + 1 }
-                else -> 0
-            } }
+        repeat(1000) { step ->
+            // North
+            (0..<size).forEach { x ->
+                var swapTo = 0
+                (0..<size).forEach { y ->
+                    when(this[y][x]) {
+                        'O' -> { val c = this[y][x]; this[y][x] = this[swapTo][x]; this[swapTo][x] = c; swapTo++ }
+                        '#' -> swapTo = y + 1
+                    }
+                }
+            }
+            // West
+            (0..<size).forEach { y ->
+                var swapTo = 0
+                (0..<size).forEach { x ->
+                    when(this[y][x]) {
+                        'O' -> { val c = this[y][x]; this[y][x] = this[y][swapTo]; this[y][swapTo] = c; swapTo++ }
+                        '#' -> swapTo = x + 1
+                    }
+                }
+            }
+            // South
+            (0..<size).forEach { x ->
+                var swapTo = size - 1
+                (0..<size).reversed().forEach { y ->
+                    when(this[y][x]) {
+                        'O' -> { val c = this[y][x]; this[y][x] = this[swapTo][x]; this[swapTo][x] = c; swapTo-- }
+                        '#' -> swapTo = y - 1
+                    }
+                }
+            }
+            // East
+            (0..<size).forEach { y ->
+                var swapTo = size - 1
+                (0..<size).reversed().forEach { x ->
+                    when(this[y][x]) {
+                        'O' -> { val c = this[y][x]; this[y][x] = this[y][swapTo]; this[y][swapTo] = c; swapTo-- }
+                        '#' -> swapTo = x - 1
+                    }
+                }
+            }
+            // Count
+            withIndex().sumOf { (y, line) ->
+                line.count { it == 'O' } * (size - y)
+            }.also { println("$step: Load $it") }
         }
     }?.also {
         println("Total load on north column is $it")
     }
 }
-
-fun List<CharArray>.rotate() = List(this[0].size) { y ->
-    CharArray(size) { x -> this[x][y] }
-}
-
-/** Shenanigans to read from the "res" folder instead of hardcoded path */
-fun getResourceAsText(path: String): String? =
-    object {}.javaClass.getResource(path)?.readText()
