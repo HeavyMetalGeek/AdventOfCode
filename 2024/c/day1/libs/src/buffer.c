ca186599ad9f156631ed94cd6874b4f981e0b569
Matt McKenna <fatirishman53@gmail.com>
2024-12-04 21:02:59 -0600

    Added generic buffer and CMakeLists
    - Can make buffer more generic with `memcpy`

--- 2024/c/day1/libs/src/buffer.c
+++ 2024/c/day1/libs/src/buffer.c
@@ -1,6 +1,116 @@
 #include "linked_list.h"
 #include "buffer.h"
 
+typedef struct {
+    void* data;
+    unsigned int length;
+    unsigned int capacity;
+} Buffer;
+
+int Buffer_init(Buffer** out_buf, size_t unit_size) {
+    (*out_buf) = malloc(sizeof(Buffer));
+    if (out_buf == NULL) {
+        return -1;
+    }
+    (*out_buf)->length = 0;
+    (*out_buf)->capacity = 255;
+    (*out_buf)->data = calloc((*out_buf)->capacity, unit_size);
+    if ((*out_buf)->data == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+int Buffer_init_int(Buffer** out_buf) {
+    (*out_buf) = malloc(sizeof(Buffer));
+    if ((*out_buf) == NULL) {
+        return -1;
+    }
+    (*out_buf)->length = 0;
+    (*out_buf)->capacity = 255;
+    (*out_buf)->data = calloc((*out_buf)->capacity, sizeof(int));
+    if ((*out_buf)->data == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+int Buffer_init_char(Buffer** out_buf) {
+    (*out_buf) = malloc(sizeof(Buffer));
+    if ((*out_buf) == NULL) {
+        return -1;
+    }
+    (*out_buf)->length = 0;
+    (*out_buf)->capacity = 255;
+    (*out_buf)->data = calloc((*out_buf)->capacity, sizeof(char));
+    if ((*out_buf)->data == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+void Buffer_free(Buffer** buf) {
+    free((*buf)->data);
+    (*buf)->data = NULL;
+    free(*buf);
+    *buf = NULL;
+}
+
+int Buffer_append_int(Buffer *buf, int val) {
+    if (buf->length == buf->capacity) {
+        buf->capacity *= 2;
+        buf->data = (int*)realloc(buf->data, sizeof(int) * buf->capacity);
+        if (buf->data == NULL) {
+            printf("ERROR: Unable to reallocate memory.");
+            return -1;
+        }
+    }
+    if (buf->data == NULL) {
+        printf("Data was NULL.");
+        return -1;
+    }
+    ((int*)buf->data)[buf->length] = val;
+    buf->length += 1;
+    return 0;
+}
+
+int Buffer_append_char(Buffer *buf, char val) {
+    if (buf->length == buf->capacity) {
+        buf->capacity *= 2;
+        buf->data = (char*)realloc(buf->data, sizeof(char) * buf->capacity);
+        if (buf->data == NULL) {
+            printf("ERROR: Unable to reallocate memory.");
+            return -1;
+        }
+    }
+    if (buf->data == NULL) {
+        printf("Data was NULL.");
+        return -1;
+    }
+    ((char*)buf->data)[buf->length] = val;
+    buf->length += 1;
+    return 0;
+}
+
+int Buffer_append_long(Buffer *buf, long val) {
+    if (buf->length == buf->capacity) {
+        buf->capacity *= 2;
+        buf->data = (void*)realloc(buf->data, sizeof(long) * buf->capacity);
+        if (buf->data == NULL) {
+            printf("ERROR: Unable to reallocate memory.");
+            return -1;
+        }
+    }
+    if (buf->data == NULL) {
+        printf("Data was NULL.");
+        return -1;
+    }
+    ((long*)buf->data)[buf->length] = val;
+    buf->length += 1;
+    return 0;
+}
+
+
 ValBuffer ValBuffer_init() {
     ValBuffer buf;
     buf.length = 0;
